47b27f8b8029fefc7b4227c0b83c9211d5fdbb20
Ana Caroline <113220888+carolnogueira13@users.noreply.github.com>
2022-10-29 10:30:18 -0300

    Create ex007.php

---
+++ Aula-29-10/ex007.php
@@ -0,0 +1,13 @@
+<?php
+$idioma = "Português";
+switch ($idioma) {
+  case "Português";
+  echo ("Bem-vindo!");
+  break;
+  case "Inglês";
+  echo ("Welcome!");
+  break;
+  case "Espanhol";
+  echo ("Bienvenido!");
+  break;
+}
