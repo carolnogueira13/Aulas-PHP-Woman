98cd028b548e0f789aa73c4b2cb37d3ec35cb078
Ana Caroline <113220888+carolnogueira13@users.noreply.github.com>
2022-10-29 12:43:51 -0300

    Update ex014.php

--- Aula-29-10/ex014.php
+++ Aula-29-10/ex014.php
@@ -2,5 +2,5 @@
 #DESAFIO TABUADA
 $num1 = readline("Digite um número para saber a tabuada dele: ");
 for($i=1; $i <= 10; $i++) {
-    echo ($num1 ." X $i= " .$num1 * $i .PHP_EOL);
+    echo ("$num1 X $i= " .$num1 * $i .PHP_EOL);
 }
